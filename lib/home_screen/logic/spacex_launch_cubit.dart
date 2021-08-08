ebf3cb8080da5c2675b0ca0924c3c7709d5bb205
salih <salihcn.bnb@gmail.com>
2021-08-08 13:16:32 +0300

    Create skelton logic layer

---
+++ lib/home_screen/logic/spacex_launch_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:spacex_latest_launch/data/repository/spacex_launch_repository.dart';
+
+part 'spacex_launch_state.dart';
+
+class SpacexLaunchCubit extends Cubit<SpacexLaunchState> {
+  SpacexLaunchCubit({required SpaceXLaunchRepository launchRepository})
+      : _launchRepository = launchRepository,
+        super(SpacexLaunchInitial());
+
+  final SpaceXLaunchRepository _launchRepository;
+
+  Future<void> fetchLatestLaunchData() async {}
+}
---
+++ lib/home_screen/logic/spacex_launch_state.dart
@@ -0,0 +1,30 @@
+part of 'spacex_launch_cubit.dart';
+
+abstract class SpacexLaunchState extends Equatable {
+  const SpacexLaunchState();
+}
+
+class SpacexLaunchInitial extends SpacexLaunchState {
+  @override
+  List<Object> get props => [];
+}
+
+class SpacexLaunchLoading extends SpacexLaunchState {
+  @override
+  List<Object> get props => [];
+}
+
+class SpacexLaunchLoaded extends SpacexLaunchState {
+  // todo add model of api data response
+  @override
+  List<Object> get props => [];
+}
+
+class SpacexLaunchError extends SpacexLaunchState {
+  final String message;
+
+  const SpacexLaunchError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
