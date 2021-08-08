ccfccc25871478dc231a926394b8be7cc84405e9
salih <salihcn.bnb@gmail.com>
2021-08-08 16:39:01 +0300

    SpaceX API getlatestLaunch method create and complete

--- lib/data/data_providers/remote/spacex_api.dart
+++ lib/data/data_providers/remote/spacex_api.dart
@@ -1,4 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class SpaceXAPI {
-  final _baseURL = 'https://api.spacexdata.com';
-  // todo singleten spacex api will create
+  final _baseURL = 'api.spacexdata.com';
+
+  Future<String> getLatestLaunch() async {
+    final url = Uri.https(_baseURL, '/v4/launches/latest');
+    final response = await http.get(url);
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to Retrieve Data: ${response.body}');
+    }
+    return response.body;
+  }
 }
