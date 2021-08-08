87201e0c4f9b6316f65cf962d5073c454c7fc955
salih <salihcn.bnb@gmail.com>
2021-08-08 16:39:51 +0300

    SpaceX launch repository use case complete

--- lib/data/repository/spacex_launch_repository_impl.dart
+++ lib/data/repository/spacex_launch_repository_impl.dart
@@ -1,13 +1,16 @@
+import 'package:spacex_latest_launch/data/data_providers/remote/spacex_api.dart';
+import 'package:spacex_latest_launch/data/models/launch_model.dart';
+
 import 'spacex_launch_repository.dart';
 
 class SpaceXLaunchRepositoryImpl implements SpaceXLaunchRepository {
-  static final SpaceXLaunchRepositoryImpl _singleton = SpaceXLaunchRepositoryImpl._internal();
-  factory SpaceXLaunchRepositoryImpl() {
-    return _singleton;
+  final SpaceXAPI _spaceXAPI;
+
+  SpaceXLaunchRepositoryImpl({required SpaceXAPI spaceXAPI}) : _spaceXAPI = spaceXAPI;
+
+  @override
+  Future<LaunchModel> getLatestLaunch() async {
+    final response = await _spaceXAPI.getLatestLaunch();
+    return LaunchModel.fromRawJson(response);
   }
-  SpaceXLaunchRepositoryImpl._internal();
-
-
-// todo create data getter and decoder methods
 }
-
