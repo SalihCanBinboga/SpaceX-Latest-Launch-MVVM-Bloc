d922065ebdaee9cd82ffd740fe7eb5a6dd554abf
salih <salihcn.bnb@gmail.com>
2021-08-08 14:06:49 +0300

    create singleton architecture

--- lib/data/repository/spacex_launch_repository_impl.dart
+++ lib/data/repository/spacex_launch_repository_impl.dart
@@ -1,5 +1,13 @@
 import 'spacex_launch_repository.dart';
 
 class SpaceXLaunchRepositoryImpl implements SpaceXLaunchRepository {
-  // todo create data getter and decoder methods
+  static final SpaceXLaunchRepositoryImpl _singleton = SpaceXLaunchRepositoryImpl._internal();
+  factory SpaceXLaunchRepositoryImpl() {
+    return _singleton;
+  }
+  SpaceXLaunchRepositoryImpl._internal();
+
+
+// todo create data getter and decoder methods
 }
+
