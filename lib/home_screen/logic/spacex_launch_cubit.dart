f58a5f11954fb7199fda814d8a37c2213426e7db
salih <salihcn.bnb@gmail.com>
2021-08-08 13:34:38 +0300

    implement launch data class to launch state

--- lib/home_screen/logic/spacex_launch_cubit.dart
+++ lib/home_screen/logic/spacex_launch_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:spacex_latest_launch/data/models/launch_model.dart';
 import 'package:spacex_latest_launch/data/repository/spacex_launch_repository.dart';
 
 part 'spacex_launch_state.dart';
