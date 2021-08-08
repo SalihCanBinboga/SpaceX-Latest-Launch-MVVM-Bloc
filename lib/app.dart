f1da49e39cb3a8d49530a16567b27a4f692cf29d
salih <salihcn.bnb@gmail.com>
2021-08-08 12:32:06 +0300

    app is into parts and create skelton

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:spacex_latest_launch/home_screen/home_screen.dart';
+
+class App extends MaterialApp {
+  App() : super(home: HomeScreen());
+}
